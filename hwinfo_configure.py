7293eea5cabe9c10f6dcb63718c86a7aebe1824d
Hunter <112955296+HunterAnnan@users.noreply.github.com>
2022-09-30 17:31:14 +0100

    Added hwinfo_configure (outline only)
    This will accept user inputs and store them. This allows a user to keep a configuration, re-configuring only when desired, instead of configuring every time or delving into the Python code.

---
+++ hwinfo_configure.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 30 17:28:59 2022
+
+Tool for configuring the hwinfo_plot tool.
+
+@author: Hunter.Annan
+"""
+
+# Accept user inputs and store them using configparser or json.
