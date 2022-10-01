916ca9dd8383a199f2f2792a80a37d473e6fcb55
Hunter <112955296+HunterAnnan@users.noreply.github.com>
2022-10-01 21:27:27 +0100

    Basic config outline for filename added

--- hwinfo_configure.py
+++ hwinfo_configure.py
@@ -7,4 +7,20 @@ Tool for configuring the hwinfo_plot tool.
 @author: Hunter.Annan
 """
 
-# Accept user inputs and store them using configparser or json.
+import json
+
+#think this needs to have some sort of "does a .json already exist...? If so,
+#import it and use it as a starting point...
+
+config_raw = {'filename' : None,
+              }
+
+# accept user input for filename & check for .csv extension
+filename_temp = input('Enter the filename of the input CSV:\n')
+if filename_temp.endswith('.csv'):
+    config_raw['filename'] = filename_temp
+else:
+    config_raw['filename'] = str(filename_temp + '.csv')
+
+# json.dumps(config_raw, indent=4)
+#some sort of save? 
