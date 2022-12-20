c918057052ee3723f762e8eb9d2609d69519a54b
Hunter <112955296+HunterAnnan@users.noreply.github.com>
2022-12-20 17:09:19 +0000

    Configures both single- and multi-var plots now

--- hwinfo_configure.py
+++ hwinfo_configure.py
@@ -22,7 +22,8 @@ def get_cfg(cfg_filename):
     except FileNotFoundError:
         cfg = {'filename' : None,
                'variables' : None,
-                  }
+               'variables_(multi)' : None
+               }
         print("No config file found. Defaults created:")
         print(cfg)
     else:
@@ -34,13 +35,13 @@ def q_reconfig(existing_cfg_file):
     Outputs proceed bool"""
     print("Existing config file found:")
     for key, val in existing_cfg_file.items():
-        print(f"{key.title() + ':  ':<15}" + str(val))
+        print(f"{key.title() + ':  ':<20}" + str(val))
     proceed_str = input("Would you like to reconfigure? Y/N:\n")
     proceed = False
     if proceed_str.lower() == "y":
         proceed = True
         print("Proceeding with reconfiguration...")
-    elif proceed_str.lower() == "n":#
+    elif proceed_str.lower() == "n":
         print("Abandoning reconfiguration")
     else:
         print("Input not recognised: abandoning reconfiguration.")
@@ -70,18 +71,23 @@ def cfg_input_CSV_filename():
     else:
         cfg['filename'] = str(input_CSV_filename + '.csv')
 
-def cfg_vars_to_plot():
+def get_available_variables():
     full_vars_list = list(get_vars
                           (
                               input_filename, var_types=['all'], silent=True).keys()
                           )
-    # full_vars_list = list(full_vars_dict.keys())
     print("\n  Available variables:")
     for i, var in enumerate(full_vars_list):
         print(i, var)
+    return full_vars_list
+
+def cfg_vars_for_single_var_plot():
+    full_vars_list = get_available_variables()
     ## Ask for some variables to plot (referenced by number) ...
-    var_indices_str = input("\nEnter the variable(s) you would like from the list above." +
-                            "\nUse the index numbers, separated by commas:\n")
+    var_indices_str = input("\nSingle-variable plots:" +
+                            "\n Enter the variable(s) you would like from the list above." +
+                            "\n Use the index numbers, separated by commas (,):\n"
+                            )
     vars_indices_list = map(int,
                             var_indices_str.replace(" ", "").split(",")
                             )
@@ -97,6 +103,38 @@ def cfg_vars_to_plot():
         print("\nStoring the following variables for plotting:")
     for var in vars_list:
         print(var)
+        
+def cfg_vars_for_multi_var_plot():
+    full_vars_list = get_available_variables()
+    ## Ask for some variables to plot (referenced by number) ...
+    var_indices_str = input("\nMulti-variable plots:" +
+                            "\n Enter the variable(s) you would like from the list above." +
+                            "\n Use the index numbers, separating variables with commas (,)" +
+                            "\n and separating plots with forward slashes (/):\n"
+                            ) 
+    vars_indices_list = list(map(str,
+                                 var_indices_str.replace(" ", "").split("/")
+                                 )
+                             )
+    plots_list = [ [] for _ in range(len(vars_indices_list)) ]
+    for plot_index, plot_vars in enumerate(vars_indices_list):
+        vars_indices_list[plot_index] = list(map(int,
+                                        vars_indices_list[plot_index].replace(" ", "").split(",")
+                                        )
+                                    )
+        for var in vars_indices_list[plot_index]:
+            print(full_vars_list[var])
+            plots_list[plot_index].append(full_vars_list[var])
+    cfg['variables_(multi)'] = plots_list
+    # feed the variables stored back to the user
+    if len(plots_list) == 1:
+        print("\nStoring variable(s) for the plot:")
+    else:
+        print("\nStoring variables for " + str(len(plots_list)) + " plots:")
+    for i, plot in enumerate(plots_list):
+        print(" Plot " + str(i) + ":")
+        for var in plot:
+            print("  " + var)
 
 if __name__ == "__main__":
     cfg_filename = 'cfg.json'
@@ -110,10 +148,14 @@ if __name__ == "__main__":
         
         #defines an input filename for cfg_vars_to_plot() to use
         input_filename = str("raw_data/" + cfg['filename'])
-            
-        proceed = q_cfg_item("variables")
+          
+        proceed = q_cfg_item("variables for single-variable plots")
         if proceed == True:
-            cfg_vars_to_plot() #adds list of variables to plot to .json
+            cfg_vars_for_single_var_plot() #adds list of variables to plot to .json
+        
+        proceed = q_cfg_item("variables for multi-variable plots")
+        if proceed == True:
+            cfg_vars_for_multi_var_plot() #adds list of variables to plot to .json
         
         #some configuration happens here
         json.dump(cfg, open(cfg_filename, 'w'), indent=4)
