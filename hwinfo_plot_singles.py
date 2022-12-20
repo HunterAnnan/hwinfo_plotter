51a5758f0e67a8d9caccd7ab86d9480d9d777483
Hunter <112955296+HunterAnnan@users.noreply.github.com>
2022-12-20 17:09:36 +0000

    Cleaned up (old plot code removed)

--- hwinfo_plot_singles.py
+++ hwinfo_plot_singles.py
@@ -82,21 +82,3 @@ if __name__ == "__main__":
           + str(round(end-start, 4))
           + " seconds."
           )
-    
-###example of a multi-plot
-# fig, ax = plt.subplots()
-# ax.scatter(data['Datetime'], data['Core Temperatures (avg) [°C]'], s=3, c='blue', label = 'CPU Avg Core Temperature')
-# ax.scatter(data['Datetime'], data['Core Thermal Throttling (avg) [Yes/No]'], s=3, c='green', label = 'Therm Throt')
-# ax.xaxis.set_major_formatter(date_fmt)
-# ax.set_ylabel('Temperature / \N{DEGREE SIGN}C')
-# ax2 = ax.twinx()
-# ax2.scatter(data['Datetime'], data['Core Effective Clocks (avg) [MHz]'], s=3, c='red', label = 'Core Clocks')
-# ax2.set_ylabel('Core Clocks / MHz')
-# ax2.set_ylim(0, 2050)
-
-# lines1, labels1 = ax.get_legend_handles_labels()
-# lines2, labels2 = ax2.get_legend_handles_labels()
-# lines, labels = (lines1 + lines2), (labels1 + labels2)
-# plt.legend(lines, labels, loc = 'lower right', scatterpoints = 7)
-
-# plt.show()
